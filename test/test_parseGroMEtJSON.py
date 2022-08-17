25b6ead48db701725899789cdc187b3575470558
Jack Ladwig <jladwig@sift.net>
2022-08-17 15:25:03 -0500

    fix resources path (overloaded value)

--- test/test_parseGroMEtJSON.py
+++ test/test_parseGroMEtJSON.py
@@ -1,14 +1,14 @@
-from importlib import resources
 from pysmt.shortcuts import get_model, And, Symbol
 from pysmt.typing import INT, REAL
 import unittest
 import os
 
+RESOURCES = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../resources")
+
 class TestCompilation(unittest.TestCase):
-    resources = os.path.join(os.path.curdir, "../resources")
 
     def test_parseGroMEt(self):
-        gFile = os.path.join(resources, "CHIME_SIR_while_loop--Gromet-FN-auto.json")
+        gFile = os.path.join(RESOURCES, "CHIME_SIR_while_loop--Gromet-FN-auto.json")
         # forall t I_t <= tau === I_0 <= tau and I_1 ...
 
 if __name__ == '__main__':
