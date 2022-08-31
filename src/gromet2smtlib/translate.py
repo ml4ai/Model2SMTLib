1b6b4e53c51e6e70b8613d7f74fb9352c57deefd
Daniel Bryce <danbryce410@gmail.com>
2022-08-31 09:58:04 -0500

    checkpoint on translation

--- src/gromet2smtlib/translate.py
+++ src/gromet2smtlib/translate.py
@@ -32,7 +32,7 @@ class QueryableGromet(object):
         Returns:
             pysmt.Node: SMTLib object for constraints.
         """
-        return self.gromet_encoding_handlers[str(self._gromet_fn.__class__)](self._gromet_fn, stack=[])
+        return self._to_smtlib(self._gromet_fn, stack=[])[0][1]
 
     def _to_smtlib(self, node, stack=[]):
         """Convert the node into a set of smtlib constraints.
@@ -47,12 +47,10 @@ class QueryableGromet(object):
 
     def _gromet_fnmodule_to_smtlib(self, node, stack=[]):
         stack.append((node.name, node))
-        fn_constraints = self._to_smtlib(node.fn, stack=stack)
+        (_, fn_constraints) = self._to_smtlib(node.fn, stack=stack)
         # attr_constraints = And([self._to_smtlib(attr, stack=node) for attr in node.attributes])
         stack.pop()
-        return And(fn_constraints 
-        #, attr_constraints
-        )
+        return ([Symbol(node.name, INT)], And(fn_constraints))
     
     def _gromet_fn_to_smtlib(self, node, stack=[]):
         """Convert a fn node into constraints.  The constraints state that:
@@ -68,41 +66,110 @@ class QueryableGromet(object):
         # fn.pof[i] = fn.bf[fn.pof[i].box-1]
         
         stack.append(("fn", node))
-        outputs = []
-        for i, pof in enumerate(node.pof):
-            stack.append((f"pof[{i}]", pof))
-            pof_head = self._to_smtlib(pof, stack=stack)
-            stack.pop()
-            stack.append((f"bf[{pof.box-1}]", node.bf[pof.box-1]))
-            pof_body = self._to_smtlib(node.bf[pof.box-1], stack=stack)
-            stack.pop()
-            outputs.append(Equals(pof_head, pof_body))
 
-        phi = And(outputs)
+        # Each iteration of this loop will generate a symbol
+        # and an implementation for a box function output port (pof),  
+        # which appear in outputs as pairs
+
+
+        outputs = []
+
+        for j, bf_decl in enumerate(node.bf):
+            # get all outputs for bf, store original index i
+            bf_pofs = [ (i, pof) for i, pof in enumerate(node.pof) if pof.box-1 == j]  
+
+            # get implementation for bf          
+            if hasattr(bf_decl, "contents") and bf_decl.contents:
+                bf_impl = stack[0][1].attributes[bf_decl.contents-1]
+            else:
+                bf_impl = bf_decl
+            stack.append((f"bf[{j}]", bf_impl))
+            [(bf_opo_symbols, phi_bf_impl)] = self._to_smtlib(bf_impl, stack=stack)
+            stack.pop()
+        
+            # Bind the opo ports of the box function to the pofs of node
+            port_bindings = []
+            pof_symbols = []
+            for i, (i_orig, pof) in enumerate(bf_pofs):
+                # pof = bf.opo
+                stack.append((f"pof[{i_orig}]", pof))
+                [([pof_head], _)] = self._to_smtlib(pof, stack=stack)
+                stack.pop()
+                pof_symbols.append(pof_head)
+                bf_opo = bf_opo_symbols[i]
+                phi_bind_pof_opo = Equals(pof_head, bf_opo)
+                port_bindings.append(phi_bind_pof_opo)
+
+                if hasattr(pof, "name") and pof.name:
+                    stack.append(f"{pof.name}", pof.name)
+                    pof_name = Symbol(self._get_stack_identifier(stack), INT)
+                    stack.pop()
+                    pof_name_pof_binding = Equals(pof_name, pof_head)
+                    port_bindings.append(pof_name_pof_binding)
+
+            phi = And(port_bindings + [phi_bf_impl])
+            # symbol = Symbol(self._get_stack_identifier(stack), INT)
+            outputs.append((pof_symbols, phi))
+
+        # TODO implement the case for inputs
+        inputs = []
+
+        # Each iteration of the following loop will generate a
+        # symbol and implementation for each outer output port (opo)
+        # in terms of the wires in wfopo.
+
+        opo_wires = []
+        if node.wfopo:
+            for i, wfopo in enumerate(node.wfopo):
+                source_opo = node.opo[wfopo.src-1]
+                target_pof = node.pof[wfopo.tgt-1]
+                
+                stack.append((f"opo[{wfopo.src-1}]", source_opo))
+                source_symbol = Symbol(self._get_stack_identifier(stack), INT)
+                stack.pop()
+                stack.append((f"pof[{wfopo.tgt-1}]", target_pof))
+                target_symbol = Symbol(self._get_stack_identifier(stack), INT)
+                stack.pop()
+
+                phi = Equals(source_symbol, target_symbol)
+                opo_wires.append(([source_symbol], phi))
+
+        # TODO implement case for input wires
+        opi_wires = []
+
         stack.pop()
-        return phi
+
+        # consolidate formulas for internals of function
+        # need opo and opi symbols for binding and the
+        # implementation of the internals
+        out_symbols = [s for (ss, _) in opo_wires for s in ss]
+        in_symbols =  [s for (ss, _) in opi_wires for s in ss]
+        impl = And([imp for (_, imp) in outputs + inputs + opo_wires + opi_wires])
+
+        return [(out_symbols+in_symbols, impl)]
 
     def _gromet_port_to_smtlib(self, node, stack=[]):
-        stack.append((node.name, node))
+        # stack.append((node.name, node))
         name = self._get_stack_identifier(stack)
-        stack.pop()
-        return Symbol(f"{name}", REAL)
+        # stack.pop()
+        return [([Symbol(f"{name}", INT)], None)]
 
     def _gromet_box_function_to_smtlib(self, node, stack=[]):
         phi = None
         if node.function_type == FunctionType.EXPRESSION:
             function_node = stack[0][1].attributes[node.contents-1]
             stack.append((f"fn", function_node))
-            phi = self._to_smtlib(function_node, stack=stack)
+            [(symbols, phi)] = self._to_smtlib(function_node, stack=stack)
             stack.pop()
         elif node.function_type == FunctionType.LITERAL:
             function_node = node.value
             stack.append((f"value", function_node))
-            phi = self._to_smtlib(function_node, stack=stack)
+            [(symbols, phi)] = self._to_smtlib(function_node, stack=stack)
+            stack.pop()
         else:
             raise ValueError(f"node.function_type = {node.function_type} is not supported.")
 
-        return phi
+        return [(symbols, phi)]
 
     def _gromet_typed_value_to_smtlib(self, node, stack=[]):
         phi = None
@@ -111,10 +178,10 @@ class QueryableGromet(object):
             # map i/o ports 
             #  wfopo
             stack.append((f"value", node.value))
-            phi = self._to_smtlib(node.value, stack=stack)
+            [(symbols, phi)] = self._to_smtlib(node.value, stack=stack)
             stack.pop()
 
-        return phi
+        return [(symbols, phi)]
 
     def _gromet_literal_value_to_smtlib(self, node, stack=[]):
         value_type = node.value_type
@@ -130,7 +197,7 @@ class QueryableGromet(object):
         literal = Symbol(f"{self._get_stack_identifier(stack)}", value_enum)
         phi = Equals(literal, value)
         
-        return phi
+        return [([literal], phi)]
 
     # STUB This is where we will read in an process the gromet file
     def query(this, query_str):
