01e6340ce7e96377a900e93ad5e79e22763f891c
Daniel Bryce <danbryce410@gmail.com>
2022-11-17 10:07:11 -0600

    black formatting

--- test/test_read_bilayer.py
+++ test/test_read_bilayer.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import unittest
-sys.path.append('/Users/dmosaphir/SIFT/Projects/ASKEM/code/model2smtlib/src')
+
+sys.path.append("/Users/dmosaphir/SIFT/Projects/ASKEM/code/model2smtlib/src")
 from model2smtlib.bilayer.translate import Bilayer
 
 DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data")
@@ -15,8 +16,10 @@ class TestCompilation(unittest.TestCase):
         bilayer = Bilayer.from_json(bilayer_json_file)
         assert bilayer
 
-#        encoding = bilayer.to_smtlib_timepoint(2) ## encoding at the single timepoint 2
-        encoding = bilayer.to_smtlib([2.5,3,4, 6]) ## encoding at the list of timepoints [2,3]
+        #        encoding = bilayer.to_smtlib_timepoint(2) ## encoding at the single timepoint 2
+        encoding = bilayer.to_smtlib(
+            [2.5, 3, 4, 6]
+        )  ## encoding at the list of timepoints [2,3]
         assert encoding
 
 
